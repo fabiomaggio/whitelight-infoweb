5f2a4a4defc574b300f7096dbb4ba9d3c16b4910
Fabio Maggio <fabio@infoweb.be>
2014-08-25 15:18:37 +0200

    Enable swipe event on picture carousel

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-    //$('.carousel').carousel();    
+    // Initialize the pictures carousel
+    $('#pictures-carousel').carousel({
+        interval: false
+    });
+    
+    //Enable swiping...
+    $(".carousel-inner").swipe({
+        //Generic swipe handler for all directions
+        swipeLeft:function(event, direction, distance, duration, fingerCount) {
+            $(this).parent().carousel('next'); 
+        },
+        swipeRight: function() {
+            $(this).parent().carousel('prev'); 
+        },
+        //Default is 75px, set to 0 for demo so any distance triggers swipe
+        threshold:0
+    });    
 });
